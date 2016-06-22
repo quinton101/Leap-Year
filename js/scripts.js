aec0730f70736e11b2452c139424966a7ea025ab
Joel Waage and Quinton Williams <joel+quinton@>
2016-06-22 10:22:37 -0700

    separate business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var leapYear = function(year) {
-      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-        return true;
-      } else {
-        return false;
-      }
-    };
+    var year = parseInt($("input#year").val());  
     var result = leapYear(year);
     $("#result").text(result);
   });
