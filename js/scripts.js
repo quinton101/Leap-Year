9fa87f661bb3c25063fff1aa8341d1844f67a62c
Joel Waage and Quinton Williams <joel+quinton@>
2016-06-22 09:56:21 -0700

    first spec test works

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
+    var leapYear = function(year) {
+      return false;
+    };
     var result = leapYear(year);
     $("#result").text(result);
   });
