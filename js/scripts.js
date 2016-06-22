8734095aa2c02b4d2f7857454f6ff38eef0e2618
Joel Waage and Quinton Williams <joel+quinton@>
2016-06-22 10:02:11 -0700

    second spec test works

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var leapYear = function(year) {
+      if (year % 4 ===0){
+        return true;
+      } else {
       return false;
+      }
     };
     var result = leapYear(year);
     $("#result").text(result);
